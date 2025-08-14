b9c4a580c97fe74241cb1d405b7ca76c38116feb
aryan-spanda <aryan.pola@spanda.ai>
2025-08-14 18:48:13 +0530

    testing after adding credentials in config repo

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -6,7 +6,7 @@ import Home from './pages/Home';
 import Users from './pages/Users';
 import About from './pages/About';
 
-// Testing controlled ArgoCD Image Updater workflow - Aug 6, 2025 
+// Testing controlled ArgoCD Image Updater workflow 
 const AppContainer = styled.div`
   min-height: 100vh;
   background-color: #f8f9fa;
